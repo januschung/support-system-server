5b087a6c416aa8c9142c3e043d0135ae4cc5b9b3
januschung <januschung@gmail.com>
2020-10-23 15:17:35 -0700

    Add files via upload

---
+++ src/main/java/com/example/supportsystemserver/SupportSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.supportsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportSystemServerApplication.class, args);
+    }
+
+}
