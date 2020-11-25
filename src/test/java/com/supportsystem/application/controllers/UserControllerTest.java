3dd922cfeac4f323976f4ac5572485da5a2994d7
januschung <januschung@gmail.com>
2020-11-25 10:18:44 -0800

    fix build error

--- src/test/java/com/supportsystem/application/controllers/UserControllerTest.java
+++ src/test/java/com/supportsystem/application/controllers/UserControllerTest.java
@@ -5,17 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -24,7 +19,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.supportsystem.application.dtos.UserDTO;
 import com.supportsystem.application.services.UserService;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 	
