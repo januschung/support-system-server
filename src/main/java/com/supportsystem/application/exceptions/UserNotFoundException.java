f345654bd73ab8f072e83923f7fd2519b611c47f
januschung <januschung@gmail.com>
2020-11-25 11:25:13 -0800

    Make basic USER logic (#4)
    * Make basic USER logic
    
    * add unit test to user
    
    * add test file
    
    * fix build error

---
+++ src/main/java/com/supportsystem/application/exceptions/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.supportsystem.application.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(Long id) {
+		super("Could not find user " + id);
+	}
+}
---
+++ src/main/java/com/supportsystem/application/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.supportsystem.application.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.supportsystem.application.domains.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
