492f849e55c538e999161827f65d64c85ed55bb6
januschung <januschung@gmail.com>
2024-04-27 23:25:31 -0700

    Fix ticket logic (#16)

--- src/main/java/com/supportsystem/application/request/dtos/TicketRequestDTO.java
+++ src/main/java/com/supportsystem/application/request/dtos/TicketRequestDTO.java
@@ -2,6 +2,9 @@ package com.supportsystem.application.request.dtos;
 
 import java.util.Date;
 
+import com.supportsystem.application.shared.Status;
+import com.supportsystem.application.shared.Status.Resolution;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -14,11 +17,14 @@ public class TicketRequestDTO {
 	private Long modifiedBy;
 
 	private Date lastModified;
-	
+
 	private String description;
-	
+
 	private Long assigneeId;
-	
+
 	private Long clientId;
 
+	private Status.Ticket status;
+
+	private Status.Resolution resolution;
 }
